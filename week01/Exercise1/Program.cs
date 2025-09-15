53e2b48ad5dc8f58f35eb15240998cf9ec53a626
RoosvelthVazquez <roosvazquez1621@gmail.com>
2025-09-15 12:03:17 -0600

    Exercise 1 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // The program asks for a full name
+        Console.Write("What is your first name? ");
+        string name1 = Console.ReadLine();
+        Console.Write("What is yor second name? ");
+        string name2 = Console.ReadLine();
+        Console.WriteLine($"Your name is {name2}, {name1} {name2}.");
     }
 }
