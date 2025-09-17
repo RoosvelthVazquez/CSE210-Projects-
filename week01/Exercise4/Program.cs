9d501263e4e992e98efaaeed09b6b17c34a0fab8
RoosvelthVazquez <roosvazquez1621@gmail.com>
2025-09-17 11:42:00 -0600

    Exercise 4 is done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics.Metrics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int counter = -1;
+
+        while (counter != 0)
+        {
+            Console.Write("Enter a number: ");
+            string NumberFromUser = Console.ReadLine();
+            int number = int.Parse(NumberFromUser);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}.");
+
+        float avarage = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The avarage is {avarage}.");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+                max = number;
+        }
+        Console.WriteLine($"The max is: {max}.");
     }
 }
