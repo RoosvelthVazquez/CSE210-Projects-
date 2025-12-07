3e4ccbc7582843ca6b30e4b8ba4da2778b795ddd
^RoosvelthVazquez <roosvazquez1621@gmail.com>
2025-12-07 17:18:55 -0600

    done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new  List<Shape>();
+
+        Square cuadrado = new Square(5, "blue");
+        shapes.Add(cuadrado);
+
+        Rectangle rectangulo = new Rectangle(8, 5, "black");
+        shapes.Add(rectangulo);
+
+        Circle circulo = new Circle(5.7, "yellow");
+        shapes.Add(circulo);
+
+        foreach (Shape i in shapes)
+        {
+            string color = i.GetColor();
+
+            double area = i.GetArea();
+
+            Console.WriteLine($"The {color} has an area of a {area:f2}");
+        }
     }
 }
